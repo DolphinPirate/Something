92bab75cf0a56770e1f339a50a8a1ab55e0d1c4b
Artem <medvynskyi.artem@gmail.com>
2019-06-05 14:04:38 +0300

    1st

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // System.out.println("Hello World!");
+
+    /*    int[] smalArt = {1,2,3,4,5};
+        int[][] bigArt = new int[4][3];
+
+        for (int i = 0, j =0; i < bigArt.length-1; i++, j=j+2){
+            bigArt[i][j] = j ;
+        }
+
+
+        System.out.println(bigArt[999]);*/
+
+        sum(1, 2);
+
+    }
+
+    public static int sum(int a, int b) {
+
+        return a + b;
+    }
+
+
+}
